bab4e0d0350befff5d1d8602697071eeb2366b6c
dougwyu <dougwyu@gmail.com>
2017-07-21 22:15:11 +0800

    initial commit
    version without loops and without subfolders

---
+++ 300Test_extractAllreadsfromSort.pl
@@ -0,0 +1,94 @@
+#!/usr/local/bin/perl -w
+# 
+# Writer:         wangxy <wxyang1988@126.com>
+# Program Date:   
+# Modifier:       wangxy <wxyang1988@126.com>
+# Last Modified:  
+##########################################################
+
+my $ver="1.0";
+
+use strict;
+use Getopt::Long;
+use Data::Dumper;
+use FindBin qw($Bin $Script);
+use File::Basename qw(basename dirname);
+
+#########################################################
+
+my %opts;
+GetOptions(\%opts,"id=s","o=s","h" ); 
+
+#&help()if(defined $opts{h});
+if(!defined($opts{id}) || !defined($opts{o}) ||defined($opts{h}))
+{
+	print <<"	Usage End.";
+	Description:
+		
+		Version: $ver
+
+	Usage:
+	
+		Please put all libraries (A-F) into 1 folder
+
+		-id     Input folder which contains A-F                must be given
+		
+		-o      filename for fasta file with all seqs                 must be given
+
+		-h    Help document
+
+	Usage End.
+
+	exit;
+}
+
+###############Time
+my $Time_Start;
+$Time_Start = sub_format_datetime(localtime(time()));
+print "\nStart Time :[$Time_Start]\n\n";
+################
+my $programe_dir=basename($0);
+my $ind=$opts{id};
+my $out=$opts{o};
+
+my @allfiles = glob "$ind/*/pool*/Tag*.txt";
+my $count=0;
+
+open OUT,">$out" || die "Can't create $out,$!\n";
+
+for (my $i=0;$i<@allfiles;$i++)
+{
+	my @a = split/\//,$allfiles[$i];
+	my $rep = $a[-3];
+	my $num = (split/pool/,$a[-2])[1];
+	my $tag_name = (split/\./,$a[-1])[0];
+	my ($t1,$t2) = split/_/,$tag_name;
+	if ($t1 eq $t2)
+	{
+		open IN,"$allfiles[$i]";
+		while (<IN>)
+		{
+			chomp;
+			
+			my @b = split/\t/,$_;
+			if ($b[3] == 1) {$count++;print OUT ">$rep","$num","_","$t1-$t2","_","$count\n$b[4]\n";}
+			else {for (my $k=1;$k<=$b[3];$k++) {$count++;print OUT ">$rep","$num","_","$t1-$t2","_","$count\n$b[4]\n";}}
+		}
+	}
+	
+	
+	
+}
+
+close (OUT);
+###############Time
+my $Time_End;
+$Time_End = sub_format_datetime(localtime(time()));
+print "\nEnd Time :[$Time_End]\n\n";
+
+###############Subs
+sub sub_format_datetime {#Time calculation subroutine
+	my($sec, $min, $hour, $day, $mon, $year, $wday, $yday, $isdst) = @_;
+	$wday = $yday = $isdst = 0;
+	sprintf("%4d-%02d-%02d %02d:%02d:%02d", $year+1900, $mon+1, $day, $hour, $min, $sec);
+}
