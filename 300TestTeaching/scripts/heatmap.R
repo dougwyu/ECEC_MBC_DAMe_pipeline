34712ecb6bca3714ca64b6becdbcba7692247fbe
dougwyu <dougwyu@gmail.com>
2017-08-17 17:19:17 +0100

    heatmap rewrite for teaching

--- 300TestTeaching/scripts/heatmap.R
+++ 300TestTeaching/scripts/heatmap.R
@@ -1,33 +1,24 @@
-# This loop runs through all the folders and pools within folders to calculate a heatmap.  
+# Generates a heatmap from a SummaryCounts.txt file 
+
+options(echo=TRUE) # if you want see commands in output file
+args <- commandArgs(trailingOnly = TRUE)
+print(args)
 
 
 library(dplyr)
 library(tidyr)
-folder <- c("A", "B", "C", "D", "E", "F")
 
-for(i in folder)
-  for(j in 1:3)
-{
-  folder <- i
-  poolnumber <- j
-  path_name <- file.path("/Users/Negorashi2011/Xiaoyangmiseqdata/MiSeq_20170410/300Test/data/seqs", paste("folder", folder, sep=""), paste("pool", poolnumber, sep=""))
-  
-  setwd(path_name)
-  print(path_name)
-  
   pool <- read.table("SummaryCounts.txt", header=F, sep = "\t")
   pool <- pool %>% select(-V3) # delete the unique sequences column
-  poolspread <- pool %>% spread(V2, V4) # tidyr::spread
+  poolspread <- pool %>% tidyr::spread(V2, V4) 
   
   row.names(poolspread) <- poolspread$V1
   poolspread <- poolspread %>% select(-V1)
   poolspread <- data.matrix(poolspread)
   
-  pdf(paste("heatmap_", "Folder", folder, "_Pool", poolnumber, ".pdf", sep=""))
+  pdf("heatmap.pdf")
   poolspread_heatmap <- heatmap(poolspread, Rowv=NA, Colv=NA, revC = TRUE, col = cm.colors(256), scale="column", margins=c(5,10))
   dev.off()
-}
-
 
 
 # https://flowingdata.com/2010/01/21/how-to-make-a-heatmap-a-quick-and-easy-solution/
